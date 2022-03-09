674378996a54e3d36d620742c6d24f47fc7d57e5
UrfinJus <43937436+urfinjs@users.noreply.github.com>
2022-03-09 08:29:33 +0300

    Create massa_update_bootsrap.sh

---
+++ massa_update_bootsrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt update
+apt --installed list 2>/dev/null | grep python3 || apt install python3 -y && python3 --version
+apt --installed list 2>/dev/null | grep python3-pip || apt install python3-pip -y && pip3 --version
+apt upgrade -y
+
+pip3 install toml
+
+python3 <(wget -qO- https://raw.githubusercontent.com/urfinjs/massa/main/massa_update_bootstrap.py)
+
+echo "New bootstrap list:"
+grep bootstrap_list /root/massa/massa-node/base_config/config.toml
+
+systemctl restart massad && sleep 5 && journalctl -u massad -n 25
